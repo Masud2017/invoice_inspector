6eda7050cc9e513c3a990d29334a40e3d7f8dae6
Astra <unroot@localhost.localdomain>
2020-03-22 22:31:14 -0800

    Including register feature pertially

--- authApp/models.py
+++ authApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+    company = models.TextField()
+    color = models.TextField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
+
+'''
+class profileDetail(models.Model):
+        email = models.ForeignKey(User, on_delete=models.CASCADE)
+        color = models.TextField()
+
+'''
--- authApp/views.py
+++ authApp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
 def register(request):
+    name = request.POST["name"]
     email = request.POST["user"]
     password = request.POST["pass"]
-    return render(request,'success.html',{'email':email,'password':password})
+    company = request.POST["company"]
+    color = request.POST["color"]
+    gender = request.POST["gender"]
+    
+    '''
+    nameDb = User(name=name)
+    emailDb = User(email=email)
+    '''
+    return render(request,'success.html',{'email':email,'password':password,'name':name,'company':company,'color':color,'gender':gender})
+
+def login(request):
+    if request.method == "POST":
+        pass
